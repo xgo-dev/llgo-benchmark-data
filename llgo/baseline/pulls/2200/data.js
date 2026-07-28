d6e073fd519b29c37f581a1fcd0b106c1bc42514
github-action-benchmark <github@users.noreply.github.com>
2026-07-28 12:23:26 +0800

    add Linux program binary size (customSmallerIsBetter) benchmark result for 6eca55320c6e807131d8b5ee6736b294f8e87733

--- llgo/baseline/pulls/2200/data.js
+++ llgo/baseline/pulls/2200/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785211501664,
+  "lastUpdate": 1785212606078,
   "repoUrl": "https://github.com/xgo-dev/llgo",
   "entries": {
     "Linux program binary size": [
@@ -21,6 +21,88 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/xgo-dev/llgo/commit/8882604a922d127756c83602a0ec303bd17bf0b9"
         },
         "date": 1785211397850,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "binary/cprintf/file",
+            "value": 18520,
+            "unit": "bytes"
+          },
+          {
+            "name": "binary/cprintf/text",
+            "value": 410,
+            "unit": "bytes"
+          },
+          {
+            "name": "binary/cprintf/data",
+            "value": 12843,
+            "unit": "bytes"
+          },
+          {
+            "name": "binary/cprintf/bss",
+            "value": 2641,
+            "unit": "bytes"
+          },
+          {
+            "name": "binary/println/file",
+            "value": 72128,
+            "unit": "bytes"
+          },
+          {
+            "name": "binary/println/text",
+            "value": 21225,
+            "unit": "bytes"
+          },
+          {
+            "name": "binary/println/data",
+            "value": 29061,
+            "unit": "bytes"
+          },
+          {
+            "name": "binary/println/bss",
+            "value": 1213,
+            "unit": "bytes"
+          },
+          {
+            "name": "binary/fmtprintf/file",
+            "value": 2212832,
+            "unit": "bytes"
+          },
+          {
+            "name": "binary/fmtprintf/text",
+            "value": 759353,
+            "unit": "bytes"
+          },
+          {
+            "name": "binary/fmtprintf/data",
+            "value": 981297,
+            "unit": "bytes"
+          },
+          {
+            "name": "binary/fmtprintf/bss",
+            "value": 323428,
+            "unit": "bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Li Jie",
+            "username": "cpunion",
+            "email": "cpunion@gmail.com"
+          },
+          "committer": {
+            "name": "Li Jie",
+            "username": "cpunion",
+            "email": "cpunion@gmail.com"
+          },
+          "id": "6eca55320c6e807131d8b5ee6736b294f8e87733",
+          "message": "ci: track baseline benchmarks",
+          "timestamp": "2026-07-28T01:18:52Z",
+          "url": "https://github.com/xgo-dev/llgo/commit/6eca55320c6e807131d8b5ee6736b294f8e87733"
+        },
+        "date": 1785212603017,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
